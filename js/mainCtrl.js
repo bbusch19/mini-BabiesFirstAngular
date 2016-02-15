5e160a07f96667181580dd3f5af302d0df06e492
Breiden Busch <breiden.busch@gmail.com>
2016-02-15 12:41:06 -0700

    Finished Complete Project

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,5 +2,8 @@ var app = angular.module('friendsList');
 
 app.controller('mainCtrl', function($scope) {
   $scope.friends = ['Geoff', 'Miles', 'Scott', 'Kory'];
-  
+
+  $scope.addFriend = function() {
+    $scope.friends.push($scope.newFriend);
+  }
 });
